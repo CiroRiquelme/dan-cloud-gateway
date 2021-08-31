484edbf7f8fec8dc893e224b2340a5786d2e7d8e
CiroRiquelmee <ciroriquelme05@gmail.com>
2021-08-31 01:21:09 -0300

    seguridad implementada

--- src/main/java/utn/isi/dan/gateway/DanCloudGatewayApplication.java
+++ src/main/java/utn/isi/dan/gateway/DanCloudGatewayApplication.java
@@ -3,13 +3,20 @@ package utn.isi.dan.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.web.bind.annotation.CrossOrigin;
+
+import org.springframework.web.bind.annotation.RestController;
+
 
 @SpringBootApplication
 @EnableDiscoveryClient
+@RestController
+@CrossOrigin(origins = "*", allowCredentials = "true", allowedHeaders = "*")
 public class DanCloudGatewayApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DanCloudGatewayApplication.class, args);
 	}
+	
 
 }
